bbc44842e68ea2f6d0c38e793fb9ff4ccf9954ad
Ben Alkov <ben.alkov@gmail.com>
2022-06-19 18:37:26 -0400

    Refactor
    - Pythonize
    - Smarter recolor
    - Smarter choose_orbit
    - Proper resize handling
    - Simplify a bit
    - Mouse click visualization easter-eggs
    
    Signed-off-by: Ben Alkov <ben.alkov@gmail.com>

--- pages/orbitingcubes-pyscript-threejs/orbitingcubes.py
+++ pages/orbitingcubes-pyscript-threejs/orbitingcubes.py
@@ -16,14 +16,11 @@ import math
 
 # Not strictly necessary, but seeing naked e.g. `document`, `window`, etc. really bothers me
 import js
-
 from pyodide import create_proxy
-
 from three import (
     AmbientLight,
     BoxBufferGeometry,
     Color,
-    ColorManagement,
     DirectionalLight,
     DoubleSide,
     EdgesGeometry,
@@ -35,22 +32,22 @@ from three import (
     Object3D,
     PerspectiveCamera,
     Scene,
-    Vector2,
     Vector3,
     WebGLRenderer,
 )
 
 import utils
 
-WIDTH = js.window.innerWidth
-HEIGHT = js.window.innerHeight
-SCENE = None
-AMB_LIGHT = None
-LIGHT = None
-CAMERA = None
-RENDERER = None
+_AMB_LIGHT = None
+_CAMERA = None
+_CLICKED = 0
+_HEIGHT = js.window.innerHeight
+_LIGHT = None
+_RENDERER = None
+_SCENE = None
+_WIDTH = js.window.innerWidth
 
-cubes = None
+_CUBES = None
 
 
 class Cube():
@@ -68,211 +65,212 @@ class Cube():
     SELF_ROT_TOLERANCE = 0.5
 
     # Pythagoras in 3D
-    _cube_max_extent = math.sqrt(3) * CUBE_MAX_SIZE
+    CUBE_MAX_EXTENT = math.sqrt(3) * CUBE_MAX_SIZE
 
     # First orbit is a little larger than the diagonal extent of the largest cube
-    _first_orbit = _cube_max_extent + (CUBE_MAX_SIZE * 0.5)
-    ORBITS = (_first_orbit, _first_orbit * 2,
-              _first_orbit * 3, _first_orbit * 4)
+    FIRST_ORBIT = CUBE_MAX_EXTENT + (CUBE_MAX_SIZE * 0.5)
+    ORBITS = (FIRST_ORBIT, FIRST_ORBIT * 2,
+              FIRST_ORBIT * 3, FIRST_ORBIT * 4)
 
     def __init__(self):
-        self._size = utils.randFloat(Cube.CUBE_MIN_SIZE, Cube.CUBE_MAX_SIZE)
-        self._position, self._radius = Cube._positionOnOrbit()
-        self._angle = self._updateAngle()
-        self._rotation = Euler.new(0.0, 0.0, utils.randFloat(0.0, math.tau))
-        self._orbitAngularSpeed = utils.avoidZero(Cube.ORBIT_SPEED_LIMIT,
-                                                  Cube.ORBIT_SPEED_TOLERANCE)
-        self._objectAngularSpeed = utils.avoidZero(Cube.SELF_ROT_SPEED_LIMIT,
-                                                   Cube.SELF_ROT_TOLERANCE)
-        self._cubeGeometry = BoxBufferGeometry.new(self._size, self._size, self._size)
-        self._outlineGeometry = EdgesGeometry.new(self._cubeGeometry)
-        self._cubeMaterial = MeshLambertMaterial.new(
+        self._size = utils.rand_float(self.CUBE_MIN_SIZE, self.CUBE_MAX_SIZE)
+        self._position, self._radius = Cube._position_on_orbit()
+        self._angle = self._update_angle()
+        self._rotation = Euler.new(0.0, 0.0, utils.rand_float(0.0, math.tau))
+        self._orbit_angular_speed = utils.avoid_zero(self.ORBIT_SPEED_LIMIT,
+                                                     self.ORBIT_SPEED_TOLERANCE)
+        self._object_angular_speed = utils.avoid_zero(self.SELF_ROT_SPEED_LIMIT,
+                                                      self.SELF_ROT_TOLERANCE)
+        self._cube_geometry = BoxBufferGeometry.new(self._size, self._size, self._size)
+        self._outline_geometry = EdgesGeometry.new(self._cube_geometry)
+        alpha = utils.map_linear(self._radius, self.ORBITS[1] - 2, self.ORBITS[3], 1.0, 0.5)
+        self._cube_material = MeshLambertMaterial.new(
             transparent=True,
             side=DoubleSide,
-
-            # More transparent *away from* the origin
-            opacity=utils.mapLinear(self._radius * 0.8,
-                                    Cube.ORBITS[0],  Cube.ORBITS[3], 1.0, 0.1)
-        )
-        self._outlineMaterial = LineBasicMaterial.new(
+            opacity=alpha)
+        self._outline_material = LineBasicMaterial.new(
             transparent=True,
             side=DoubleSide,
-
-            # More transparent *toward* the origin
-            opacity=utils.mapLinear(self._radius * 0.8,
-                                    Cube.ORBITS[0],  Cube.ORBITS[3], 0.6, 1.0)
-        )
-        self._outlineMesh = LineSegments.new(self._outlineGeometry,
-                                             self._outlineMaterial)
-        self._cubeMesh = Mesh.new(self._cubeGeometry,
-                                  self._cubeMaterial)
-        self._cubeMesh.position.copy(self._position)
-        self._cubeMesh.rotation.copy(self._rotation)
-        self._cubeMesh.add(self._outlineMesh)
+            opacity=alpha)
+        self._outline_mesh = LineSegments.new(self._outline_geometry,
+                                              self._outline_material)
+        self._cube_mesh = Mesh.new(self._cube_geometry, self._cube_material)
+        self._cube_mesh.position.copy(self._position)
+        self._cube_mesh.rotation.copy(self._rotation)
+        self._cube_mesh.add(self._outline_mesh)
         self.recolor()
 
-    def _updateAngle(self):
-        position = Vector2.new(self._position.x, self._position.y)
+    def _update_angle(self):
+        return math.atan2(self._position.y, self._position.x)
 
-        # Returns radians!
-        return position.angle()
-
-    def getMeshObject(self):
-        return self._cubeMesh
+    def get_mesh_object(self):
+        return self._cube_mesh
 
     def orbit(self):
-        x = self._position.x
-        y = self._position.y
-        theta = math.radians(self._orbitAngularSpeed)
-        self._position.x = x * math.cos(theta) + y * math.sin(theta)
-        self._position.y = y * math.cos(theta) - x * math.sin(theta)
-        self._cubeMesh.position.copy(self._position)
-        self._angle = self._updateAngle()
+        x_pos = self._position.x
+        y_pos = self._position.y
+        theta = math.radians(self._orbit_angular_speed)
+        self._position.x = x_pos * math.cos(theta) + y_pos * math.sin(theta)
+        self._position.y = y_pos * math.cos(theta) - x_pos * math.sin(theta)
+        self._cube_mesh.position.copy(self._position)
+        self._angle = self._update_angle()
 
     def rotate(self):
-        self._rotation.x += math.radians(self._objectAngularSpeed)
-        self._rotation.y += math.radians(self._objectAngularSpeed)
-        self._rotation.z += math.radians(self._objectAngularSpeed)
-        self._cubeMesh.rotation.x = self._rotation.x
-        self._cubeMesh.rotation.y = self._rotation.y
-        self._cubeMesh.rotation.z = self._rotation.z
+        self._rotation.x += math.radians(self._object_angular_speed)
+        self._rotation.y += math.radians(self._object_angular_speed)
+        self._rotation.z += math.radians(self._object_angular_speed)
+        self._cube_mesh.rotation.x = self._rotation.x
+        self._cube_mesh.rotation.y = self._rotation.y
+        self._cube_mesh.rotation.z = self._rotation.z
 
     def recolor(self):
-        blue = Color.new(0x2525C4)
-        green = Color.new(0x7DB528)
-        angle = math.degrees(self._angle)
-
+        blue = Color.new(0x1515eb)
+        dk_blue = Color.new(0x0a0a73)
+        green = Color.new(0x95c251)
+        dk_green = Color.new(0x394a1f)
+        angle = abs(self._angle)
+        half_pi = math.pi / 2
         # Left half
-        if (90 <= angle <= 270):
-            # 2nd quad
-            if (90 <= angle <= 180):
-                shade = utils.mapLinear(angle, 180, 90, 0.0, 0.5)
-
-            # 3rd quad
-            elif (180 < angle <= 270):
-                shade = utils.mapLinear(angle, 180, 270, 0.0, 0.5)
-
+        if angle >= half_pi:
+            shade = utils.map_linear(angle, math.pi, half_pi, 0, 0.5)
             color = green.clone()
-            otherColor = blue.clone()
-
-        # Right half
+            other_color = blue.clone()
+            outline_color = dk_green.clone()
+            other_outline_color = dk_blue.clone()
+        # Right half.
         else:
-            # 1st quad
-            if (0 <= angle < 90):
-                shade = utils.mapLinear(angle, 0, 89.99, 0.0, 0.5)
-
-            # 4th quad
-            elif (270 < angle <= 360):
-                shade = utils.mapLinear(angle, 360, 269.99, 0.0, 0.5)
-
+            shade = utils.map_linear(angle, 0, half_pi, 0, 0.5)
             color = blue.clone()
-            otherColor = green.clone()
+            other_color = green.clone()
+            outline_color = dk_blue.clone()
+            other_outline_color = dk_green.clone()
 
-        self._cubeMaterial.color = color.clone()
-        self._cubeMaterial.color.lerp(otherColor.clone(),
-                                      # avoid obvious color bands
-                                      shade + utils.randFloat(-0.02, 0.02))
-        self._outlineMaterial.color = self._cubeMaterial.color.clone()
+        self._cube_material.color = color.clone()
+        self._cube_material.color.lerp(
+            other_color.clone(),
+            # avoid obvious color bands
+            shade + utils.rand_float(-0.02, 0.02))
+        self._outline_material.color = outline_color.clone()
+        self._outline_material.color.lerp(
+            other_outline_color.clone(),
+            # avoid obvious color bands
+            shade + utils.rand_float(-0.02, 0.02))
 
-        # Outline color is 50% more saturated, 15% darker than cube color
-        self._outlineMaterial.color.offsetHSL(0.0, 0.50, -0.15)
-
-    def _chooseOrbit():
+    def _choose_orbit():
         # Randomly choose an orbit, based on a set of probabilities.
-        # The probabilities favor larger orbits, as they have more room for more cubes.
-        chance = utils.randFloat(0.0, 1.0)
+        # The probabilities favor larger orbits, as they have room for more cubes.
+        chance = utils.rand_float(0.0, 1.0)
         if chance < 0.16:
-            orbit = Cube.ORBITS[0]
-        elif chance < 0.40:
-            orbit = Cube.ORBITS[1]
-        elif chance < 0.72:
-            orbit = Cube.ORBITS[2]
-        elif chance < 1.0:
-            orbit = Cube.ORBITS[3]
-        return orbit
+            return Cube.ORBITS[0]
+        if chance < 0.40:
+            return Cube.ORBITS[1]
+        if chance < 0.72:
+            return Cube.ORBITS[2]
+        if chance < 1.0:
+            return Cube.ORBITS[3]
 
-    def _positionOnOrbit():
+        return 0
+
+    def _position_on_orbit():
         # Generate a random position on the circumference of the orbit chosen for
         # this item.
-        angle = utils.randFloat(0.0, math.tau)
-        orbit = Cube._chooseOrbit()
+        angle = utils.rand_float(0.0, math.tau)
+        orbit = Cube._choose_orbit()
 
         # Randomly offset the position on the orbit, so we don't end up with multiple
         # cubes orbiting on *exactly* the same circles.
-        radius = orbit + utils.randFloat(0.0, Cube.ORBITS[0])
-        creationX = math.cos(angle) * radius
-        creationY = math.sin(angle) * radius
-        position = Vector3.new(creationX, creationY, 0)
+        radius = orbit + utils.rand_float(0.0, Cube.ORBITS[0])
+        creation_x = math.cos(angle) * radius
+        creation_y = math.sin(angle) * radius
+        position = Vector3.new(creation_x, creation_y, 0)
         return [position, radius]
 
 
-def init():
-    global SCENE
-    global AMB_LIGHT
-    global LIGHT
-    global CAMERA
-    global RENDERER
+def _handle_resize(event):
+    _CAMERA.aspect = js.window.innerWidth / js.window.innerHeight
+    _CAMERA.updateProjectionMatrix()
+    _RENDERER.setSize(js.window.innerWidth, js.window.innerHeight)
+    _RENDERER.setPixelRatio(js.window.devicePixelRatio)
 
-    py_canvas = js.document.getElementById('py_canvas')
-    py_canvas.querySelector('.loading').remove()
 
-    ColorManagement.legacyMode = False
+def _handle_click(event):
+    global _CLICKED
+    if _CLICKED == 0:
+        _CAMERA.near = 31.9
+        _CAMERA.far = 32.1
+        _CAMERA.updateProjectionMatrix()
+    if _CLICKED == 1:
+        _CAMERA.position.y = -20
+        _CAMERA.position.z = 20
+        _CAMERA.near = 15
+        _CAMERA.far = 150
+        _CAMERA.lookAt(Vector3.new(0, 0, 0))
+        _CAMERA.updateProjectionMatrix()
+    if _CLICKED == 2:
+        _CAMERA.position.y = -1
+        _CAMERA.position.z = 32
+        _CAMERA.lookAt(Vector3.new(0, 0, 0))
+        _CAMERA.updateProjectionMatrix()
+    _CLICKED = _CLICKED + 1 if _CLICKED < 2 else 0
 
-    # Global Z-up
+
+def _init():
+    global _SCENE
+    global _AMB_LIGHT
+    global _LIGHT
+    global _CAMERA
+    global _RENDERER
+
+    _SCENE = Scene.new()
+
+    # I can't find another way to do this...
     Object3D.DefaultUp = Vector3.new(0, 0, 1)
-
-    SCENE = Scene.new()
-    LIGHT = DirectionalLight.new()
-    AMB_LIGHT = AmbientLight.new()
-    CAMERA = PerspectiveCamera.new(
+    _LIGHT = DirectionalLight.new()
+    Object3D.DefaultUp = Vector3.new(0, 1, 0)
+    _AMB_LIGHT = AmbientLight.new()
+    _CAMERA = PerspectiveCamera.new(
         50,  # F.O.V.
-        WIDTH / HEIGHT,  # Aspect
+        _WIDTH / _HEIGHT,  # Aspect
         30,  # Near clip
         34  # Far clip
     )
-    # Camera Z-up
-    CAMERA.up.set(0, 0, 1)
-    dk_blue = Color.new(0x111550)
-    RENDERER = utils.rendererConfig(WebGLRenderer, WIDTH, HEIGHT, dk_blue)
+    _RENDERER = utils.renderer_config(WebGLRenderer, _WIDTH, _HEIGHT,
+                                      Color.new(0x46474c))  # Middle grey
 
 
-def setup():
-    global cubes
+def _setup():
+    global _CUBES
 
     num_cubes = 100
 
-    CAMERA.setFocalLength = 70
-    CAMERA.position.x = 0
-    CAMERA.position.y = 0
-    CAMERA.position.z = 32
-    CAMERA.lookAt(Vector3.new(0, 0, 0))
-    LIGHT.intensity = 2.0
-    amber = Color.new(0xb3a297)
-    AMB_LIGHT.color = amber
-    AMB_LIGHT.intensity = 0.6
-    SCENE.add(LIGHT)
-    SCENE.add(AMB_LIGHT)
-    cubes = [Cube() for _ in range(num_cubes)]
-    for cube in cubes:
-        SCENE.add(cube.getMeshObject())
-    js.document.body.appendChild(RENDERER.domElement)
-    js.window.requestAnimationFrame = utils.platformRequestAnimationFrame()
-    RENDERER.render(SCENE, CAMERA)
+    _CAMERA.setFocalLength = 70
+    _CAMERA.position.z = 32
+    _CAMERA.lookAt(Vector3.new(0, 0, 0))
+    _CAMERA.updateProjectionMatrix()
+    _AMB_LIGHT.color = Color.new(0xb3a297)  # amber
+    _AMB_LIGHT.intensity = 0.6
+    _SCENE.add(_LIGHT)
+    _SCENE.add(_AMB_LIGHT)
+    _CUBES = [Cube() for _ in range(num_cubes)]
+    for cube in _CUBES:
+        _SCENE.add(cube.get_mesh_object())
+    js.window.addEventListener('click', create_proxy(_handle_click))
+    js.window.addEventListener('resize', create_proxy(_handle_resize))
+    js.document.body.appendChild(_RENDERER.domElement)
+    _RENDERER.setAnimationLoop(create_proxy(_animate))
+    _RENDERER.render(_SCENE, _CAMERA)
 
 
-def update():
-    for cube in cubes:
+def _animate(*args):
+    for cube in _CUBES:
         cube.rotate()
         cube.orbit()
         cube.recolor()
+    _RENDERER.render(_SCENE, _CAMERA)
 
 
-def animate(*args):
-    js.window.requestAnimationFrame(create_proxy(animate))
-    update()
-    RENDERER.render(SCENE, CAMERA)
+pyscript_loader.close()
 
-
-init()
-setup()
-animate()
+_init()
+_setup()
+_animate()
--- pages/static/py/utils/utils.py
+++ pages/static/py/utils/utils.py
@@ -1,40 +1,41 @@
-import js
 import random
 
+import js
 
-def avoidZero(range, tolerance):
+
+def avoid_zero(range_, tolerance):
     # Return a random value in the range from `-range` to strictly less than
     # `range`, excluding the inner range +/-`tolerance` (and, logically, zero as
     # well).
-    value = randFloat(-range, range)
-    while -tolerance < value and value < tolerance:
-        value = randFloat(-range, range)
-    return value
+    attempt = rand_float(-range_, range_)
+    while -tolerance < attempt and attempt < tolerance:
+        attempt = rand_float(-range_, range_)
+    return attempt
 
 
 # Linear mapping from range [from_start, from_end] to range [to_start, to_end]
-def mapLinear(x, from_start, from_end, to_start, to_end):
+def map_linear(to_map, from_start, from_end, to_start, to_end):
     """Implementation as found in three.js
         Pending merge of https://github.com/pyodide/pyodide/pull/2520
     """
-    return to_start + (x - from_start) * (to_end - to_start) / (from_end - from_start)
+    return to_start + (to_map - from_start) * (to_end - to_start) / (from_end - from_start)
 
 
-def platformRequestAnimationFrame():
+def platform_request_animation_frame():
     return (js.window.requestAnimationFrame or
             js.window.mozRequestAnimationFrame or
             js.window.webkitRequestAnimationFrame)
 
 
 # Random float from [low, high] interval
-def randFloat(start, end):
+def rand_float(start, end):
     """Implementation as found in three.js
         Pending merge of https://github.com/pyodide/pyodide/pull/2520
     """
     return start + random.random() * (end - start)
 
 
-def rendererConfig(renderer, width, height, clearColor=0x000000):
+def renderer_config(renderer, width, height, clear_color=0x000000):
     renderer = renderer.new(
         powerPreference='high-performance',
         antialias=True,
@@ -43,5 +44,5 @@ def rendererConfig(renderer, width, height, clearColor=0x000000):
     )
     renderer.setPixelRatio(js.window.devicePixelRatio)
     renderer.setSize(width, height)
-    renderer.setClearColor(clearColor, 1.0)
+    renderer.setClearColor(clear_color, 1.0)
     return renderer
